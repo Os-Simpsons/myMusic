70e02370655c26602c19b34887b7780b0d89300c
thiagosougo1 <thiagosg@ciandt.com>
2022-08-18 19:00:52 -0300

    Artisty entity

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/entities/ArtistEntity.java
@@ -0,0 +1,33 @@
+package com.ciandt.summit.bootcamp2022.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+
+@Entity
+public class ArtistEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name="Id")
+    private String id;
+
+    @Column(name="Nome")
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
