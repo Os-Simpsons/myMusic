d52c60fefe2b02faa02b8390156fabd2eb6d1eeb
miguelmoraisjr <miguelj@ciandt.com>
2022-08-19 17:24:01 -0300

    Adicionando Lombok ao dto

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/dto/MusicDto.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/dto/MusicDto.java
@@ -1,44 +1,19 @@
 package com.ciandt.summit.bootcamp2022.dto;
 
 import com.ciandt.summit.bootcamp2022.entity.Artist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MusicDto {
 
     private String id;
     private String name;
     private Artist artist;
-
-    public MusicDto() {
-
-    }
-
-    public MusicDto(String id, String name, Artist artist) {
-        this.id = id;
-        this.name = name;
-        this.artist = artist;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Artist getArtist() {
-        return artist;
-    }
-
-    public void setArtist(Artist artistId) {
-        this.artist = artistId;
-    }
+    
 }
