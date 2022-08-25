4f07f64064a7b028048d749f761b44c98db69ef0
thiagosougo1 <thiagosg@ciandt.com>
2022-08-25 19:41:32 -0300

    Swagger almost done

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/SwaggerConfig.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/SwaggerConfig.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -22,7 +25,7 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
@@ -67,10 +70,24 @@ public class SwaggerConfig {
         };
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Profile-Management - ENGLISH TALK ")
-                .description("\"Desenvolvido por - Karin de Oliveira\"")
+                .title("MyMusic")
+                .description("\"Developed by Os Simpsons\"")
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
