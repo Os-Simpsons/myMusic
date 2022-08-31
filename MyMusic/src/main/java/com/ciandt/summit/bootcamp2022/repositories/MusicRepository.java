c1d64556f51650ca311b51893e6e59f4a62df5f0
joaobragaciandt <joaobraga@ciandt.com>
2022-08-31 09:46:01 -0300

    Criação da Cache no repositório.

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repositories/MusicRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repositories/MusicRepository.java
@@ -1,6 +1,7 @@
 package com.ciandt.summit.bootcamp2022.repositories;
 
 import com.ciandt.summit.bootcamp2022.entity.Music;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,7 @@ import java.util.List;
 @Repository
 public interface MusicRepository extends JpaRepository<Music, String> {
     @Query(value = "SELECT DISTINCT M FROM Music M INNER JOIN Artist A ON A.id =  M.artistId WHERE Upper(A.name) Like Upper ('%' || :name || '%') OR Upper(M.name) like Upper('%' || :name || '%') ORDER BY M.name, A.name")
+    @Cacheable("musics")
     public List<Music> getAllMusicArtist(String name);
 
 
