c6d0c91903c65936ef2dcacfa5b02a17bd71130d
miguelmoraisjr <miguelj@ciandt.com>
2022-09-01 11:18:45 -0300

    Alterando endereço api camila

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/client/TokenFeignClient.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/client/TokenFeignClient.java
@@ -4,7 +4,7 @@ import com.ciandt.summit.bootcamp2022.dto.UsernameDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "tokenFeignClient",url = "http://localhost:8080")
+@FeignClient(name = "tokenFeignClient",url = "https://token-provider-teste.herokuapp.com")
 public interface TokenFeignClient {
 
     @PostMapping("/api/v1/token")
