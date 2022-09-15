211787065af6ffb61827769ab5de351b4154ef90
joaobragaciandt <joaobraga@ciandt.com>
2022-09-15 11:28:07 -0300

    Implementação da cache

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/services/PlaylistServiceImpl.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/services/PlaylistServiceImpl.java
@@ -14,6 +14,7 @@ import com.ciandt.summit.bootcamp2022.utils.exceptions.InvalidLogDataException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,6 +36,7 @@ public class PlaylistServiceImpl implements PlaylistService {
     private static Logger logger = LogManager.getLogger(PlaylistServiceImpl.class);
 
     @Transactional
+    @Cacheable(cacheNames = "saveMusicToPlaylist")
     @Override
     public void saveMusicToPlaylistCheckingUserTpe(String playlistId, String userId, MusicDto musicDto){
         try{
@@ -73,6 +75,7 @@ public class PlaylistServiceImpl implements PlaylistService {
     }
 
     @Transactional
+    @Cacheable(cacheNames = "deleteMusicFromPlaylist")
     @Override
     public void deleteMusicFromPlaylist(String playlistId, String musicId) {
         try {
